2462aa318711395a49eb734922968ba9ba974626
k_waleed384 <waleedkhalid44412@gmail.com>
2023-03-26 15:10:43 +0200

    created a seperate component and called it on app.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import './App.css';
 import { Component } from 'react';
-import { BrowserRouter as Router, Routes, Route as RouterRoute } from 'react-router-dom';
-import PopulationChart from './components/populationChart/populationChart'
-import Route from './enum/route.enum';
+import PopulationChart from './components/populationChart/populationChart';
+
 class App extends Component {
   render() {
     return (
-      <Router >
-        <div className="column">
-          <div className="column">
-            {/* {Storages.loginStorage.getToken() && <HomeNavbar />} */}
-          </div>
-        </div>
-        <div className="container-fixed">
-        <div className="columns">
-        <div  className="column is-2">
-          {/* {Storages.loginStorage.getToken() && <div id="scrollableContentSidebar" > */}
-            <div className='container is-fullhd'>
-              <div className="notification is-white">
-                {/* <SideMenubar /> */}
-              </div>
-            </div>
-          </div>
-          <div  className="column is-10">
-          <div id="scrollableContent">
-                <Routes>
-                  {/* <RouterRoute exact path={Route.PopulationChart} component={PopulationChart}></RouterRoute> */}
-                  <RouterRoute exact={true} path={Route.PopulationChart} component={PopulationChart}></RouterRoute>
-
-                  {/* <RouterRoute exact component={Home}></RouterRoute> */}
-                </Routes>
-              </div>
-            </div>
-          
-          </div>
-
-        </div>
-      </Router>
+      <div>
+        <PopulationChart />
+      </div>
     );
   }
 }
